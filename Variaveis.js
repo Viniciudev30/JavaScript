2ffb73d88b4afcadc470ee4ce920ba58d92ff7d8
Vinicius Faria <156589476+Viniciudev30@users.noreply.github.com>
2025-02-07 08:09:06 -0300

    Add files via upload

---
+++ Variaveis.js
@@ -0,0 +1,9 @@
+console.log("Mexendo com Variaveis");
+ // Console.log("Mexendo com Variaveis");
+//Js e Case sensitive 
+
+const idade = 21;
+const nome ="Vinicius";
+
+// não fazer
+// mes = "Janeiro ";
