9c996b96e460a8a50cd14c100096cb71282bc5dd
Vinicius Faria <156589476+Viniciudev30@users.noreply.github.com>
2025-02-17 11:53:44 -0300

    Add files via upload

---
+++ Conversor.js
@@ -0,0 +1,8 @@
+console.log("Conversor de tipos de variaveis");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") / parseInt("2"));
+
+console.log("7" / "2");
+ // console.log("Vinicius" / "2"); // Não é um número
