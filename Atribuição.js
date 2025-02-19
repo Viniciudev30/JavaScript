598740e1742e7d155eaabd1a3fcd4b91e5928cde
Vinicius Faria <156589476+Viniciudev30@users.noreply.github.com>
2025-02-19 09:01:37 -0300

    Add files via upload

---
+++ Atribuição.js
@@ -0,0 +1,19 @@
+console.log("Mexendo com atribuição de variaveis");
+
+const primeiroNome ="Vinicius";
+const sobreNome ="Faria";
+
+ // console.log(primeiroNome + " " + sobreprimeiroNome);
+console.log(primeiroNome , sobreNome);
+console.log(`Meu primeiroNome é ${primeiroNome} ${sobreNome}`);
+
+let contador = 0
+contador = contador +1
+
+const nomeCompleto = primeiroNome + sobreNome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade =21; //atribuindo valor
+idade = idade+1;
+console.log(idade);
